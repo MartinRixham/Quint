5c2e2ae8f8761facf623f47fae2837b5bfc8aded
Martin Rixham <martin.rixham@gmail.com>
2024-08-29 22:50:45 +0100

    Test calculating kets.

--- test/expression/ketTest.js
+++ test/expression/ketTest.js
@@ -6,23 +6,12 @@ import Variable from "~/js/expression/Variable";
 
 QUnit.module("ket");
 
-QUnit.test("format ket of pi", async assert => {
+QUnit.test("calculate ket of pi", async assert => {
 
 	const ket = new Ket(new Pi());
 
 	assert.strictEqual(ket.format(), "<mrow><mo>|</mo><mi>π</mi><mo>&rang;</mo></mrow>");
-});
 
-QUnit.test("format ket of rational", async assert => {
-
-	const ket = new Ket(new Value({ value: 123.456 }));
-
-	assert.strictEqual(ket.format(), "<mrow><mo>|</mo><mn>123.456</mn><mo>&rang;</mo></mrow>");
-});
-
-QUnit.test("calculate ket of pi", async assert => {
-
-	const ket = new Ket(new Pi());
 	const result = ket.calculate();
 
 	assert.strictEqual(result.format(), `
@@ -40,9 +29,35 @@ QUnit.test("calculate ket of pi", async assert => {
       </mrow>`.replace(/\s/g,''));
 });
 
+QUnit.test("calculate ket of rational", async assert => {
+
+	const ket = new Ket(new Value({ value: 123.456 }));
+
+	assert.strictEqual(ket.format(), "<mrow><mo>|</mo><mn>123.456</mn><mo>&rang;</mo></mrow>");
+
+	const result = ket.calculate();
+
+	assert.strictEqual(result.format(), `
+      <mrow>
+        <mo>(</mo>
+        <mtable>
+          <mtr>
+            <mtd><mo>cos</mo><mfrac><mn>123.456</mn><mn>2<mn></mn></mn></mfrac></mtd>
+		  </mtr>
+	      <mtr>
+            <mtd><mo>sin</mo><mfrac><mn>123.456</mn><mn>2<mn></mn></mn></mfrac></mtd>
+          </mtr>
+        </mtable>
+        <mo>)</mo>
+      </mrow>`.replace(/\s/g,''));
+});
+
 QUnit.test("calculate ket of theta", async assert => {
 
 	const ket = new Ket(new Variable({ identifier: "theta" }));
+
+	assert.strictEqual(ket.format(), "<mrow><mo>|</mo><mi>&theta;</mi><mo>&rang;</mo></mrow>");
+
 	const result = ket.calculate();
 
 	assert.strictEqual(result.format(), `
