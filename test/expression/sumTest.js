28cc45cd422935c5ccbbdf0a5ac4edf75f2a2912
Martin Rixham <martin.rixham@gmail.com>
2024-08-29 22:35:24 +0100

    Test integer addition.

---
+++ test/expression/sumTest.js
@@ -0,0 +1,14 @@
+import QUnit from "qunit";
+import Sum from "~/js/expression/Sum";
+import Value from "~/js/expression/Value";
+
+QUnit.module("sum");
+
+QUnit.test("add integers", async assert => {
+
+	const left = new Value({ value: 1 });
+	const right = new Value({ value: 2 });
+	const sum = new Sum(left, right);
+
+	assert.strictEqual(sum.format(), "<mn>1</mn><mo>+</mo><mn>2</mn>");
+});
